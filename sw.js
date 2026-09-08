395a4699efb468d8f0f45fd9a1bc5d8b517db232
walkersevenonethreenineone <98523965+walkersevenonethreenineone@users.noreply.github.com>
2026-09-08 12:05:12 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = "boringtube-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
